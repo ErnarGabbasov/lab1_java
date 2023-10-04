610d8d128473546f650b64c542b48bad9a0f7f38
Ernar Gabbasov <ernar2272@gmail.com>
2023-10-04 20:21:55 +0600

    lab1

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; // объект класса Scanner для чтения ввода с клавиатуры.
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество строк: ");
+        int n = scanner.nextInt();
+        scanner.nextLine(); // считываем конец строки после ввода числа
+
+        String[] strings = new String[n]; // Создаем массив строк
+
+        // Ввод строк с консоли
+        for (int i = 0; i < n; i++) {
+            System.out.print("Введите строку " + (i + 1) + ": ");
+            strings[i] = scanner.nextLine();
+        }
+
+        // Вычисление средней длины строк
+        double sum = 0;
+        for (String str : strings) { // по каждой строке в массиве.
+            sum += str.length();
+        }
+        double averageLength = sum / n;
+
+        // Вывод строк, длина которых меньше средней, и их длины
+        System.out.println("Строки, длина которых меньше средней (" + averageLength + "):");
+        for (String str : strings) {
+            if (str.length() < averageLength) {
+                System.out.println(str + " (длина = " + str.length() + ")");
+            }
+        }
+    }
+}
